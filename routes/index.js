fe0974926bdacaba74638cd861d5df40680ef626
Dabana Intenque <64395601+dabanaluintenque@users.noreply.github.com>
2022-12-17 15:56:25 -0500

    change index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-
 var router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 
@@ -13,37 +12,38 @@ let db = new sqlite3.Database('./routes/Intenque_Singh.db', sqlite3.OPEN_READONL
     }   
 });
  
-  db.serialize(() => {
-    db.each(`SELECT Name as name, ProductId p_id From Product`,
-     (err, row) => {
-        if(err){
-            console.log(" No found definition")
-        }
-        console.log(row.p_id +" "+ row.name);
-     });
- });
-
- db.serialize(() => {
-    db.each(`Select * From Product Join Company using(CompanyId)`, 
-    (err, row)=>{
-        if(err){
-            console.error(err.message);
-        }
-        console.log(row);
-    })
-
-})  
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ res.render('index', { title: 'Intenque_Singh' })
+
 }); 
 
-db.close((err) => {
+/*db.close((err) => {
     if (err) {
         console.log(err.message);
     }
     console.log("Close the database connection.");
- }); 
+ }); */
 module.exports = router;
 
+
+ /*db.serialize(() => {
+   db.each(`SELECT Name as name, ProductId p_id From Product`, (err, row) => {
+     if (err) {
+       console.log(" No found definition");
+     }
+     console.log(row.p_id + " " + row.name);
+   });
+ });
+
+ db.serialize(() => {
+   db.each(
+     `Select * From Product Join Company using(CompanyId)`,
+     (err, row) => {
+       if (err) {
+         console.error(err.message);
+       }
+       console.log(row);
+     }
+   );
+ });  */
